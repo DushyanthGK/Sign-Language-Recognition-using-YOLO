409d13427530a95079d35969c1df4b0e26dd056e
DushyanthGK <103658465+DushyanthGK@users.noreply.github.com>
2024-11-20 23:17:12 +0530

    Changes

--- Sign-Language-Generation-From-Video-using-YOLOV5-master/run.py
+++ Sign-Language-Generation-From-Video-using-YOLOV5-master/run.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("python detect.py --weights best.pt --img 416 --conf 0.5 --source 0")
+os.system("python E:\Projects\Sign-Language-Generation-From-Video-using-YOLOV5-master\yolov5\content\yolov5\detect.py --weights E:\Projects\Sign-Language-Generation-From-Video-using-YOLOV5-master\yolov5\content\yolov5\runs\train\yolov5l_results4\weights\best.pt --img 416 --conf 0.5 --source 0")
